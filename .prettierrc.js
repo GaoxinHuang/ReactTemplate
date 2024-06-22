9dfd9fc71ce751a67e4627a06b5c93f29c457caa
Godfrey Huang <godfrey.Huang@lincolnuni.ac.nz>
2024-06-22 14:52:27 +1200

    Fix

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    semi: true,
-    trailingComma: 'es5',
-    singleQuote: true,
-    printWidth: 120,
-    tabWidth: 4,
-    endOfLine: 'auto', //必须要加这个。 默认是"lf", 不然有可能会引发 Delete `cr` 编译错误
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  endOfLine: 'auto', //必须要加这个。 默认是"lf", 不然有可能会引发 Delete `cr` 编译错误
 };
